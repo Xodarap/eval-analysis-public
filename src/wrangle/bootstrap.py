c894ef7567c98b7e95abe8326d36c9a6301887ea
Ben West <Xodarap@users.noreply.github.com>
2025-09-13 13:58:45 -0700

    c

--- src/wrangle/bootstrap.py
+++ src/wrangle/bootstrap.py
@@ -12,6 +12,23 @@ from src.utils.logistic import get_x_for_quantile, logistic_regression
 
 logger = logging.getLogger(__name__)
 
+from ctypes import c_double, c_int, CDLL
+import sys
+
+lib_path = 'square.so'
+basic_function_lib = CDLL(lib_path)
+
+python_c_square = basic_function_lib.c_square
+python_c_square.restype = None
+
+def do_square_using_c(list_in):
+    n = len(list_in)
+    c_arr_in = (c_double * n)(*list_in)
+    c_arr_out = (c_double * n)()
+
+    python_c_square(c_int(n), c_arr_in, c_arr_out)
+    return c_arr_out[:]
+
 
 class WrangleParams(TypedDict):
     runs_file: pathlib.Path
